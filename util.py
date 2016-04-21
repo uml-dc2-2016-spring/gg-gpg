70d688b0108e25771336c38691b3052be60bd98c
Matt Wolf <mattwollf@gmail.com>
2016-04-21 14:08:36 -0400

    popen, check_output wrappers, secret and public key getter

--- util.py
+++ util.py
@@ -1,5 +1,8 @@
 
 import os
+import subprocess as sp
+import shlex
+import re
 
 def create_channel(name, rootdir, infile='in', outfile='out'):
     """
@@ -28,3 +31,63 @@ def create_channel(name, rootdir, infile='in', outfile='out'):
 
     os.chdir(cwd)
 
+def get_secret_keys():
+    """
+        List the available secret keys that can be used for decryption or signing.
+
+        return:
+            A dictionary containing subkey and key keys, with lists of every available key list respectively
+
+    """
+    out = get_output('gpg --list-secret-keys --with-colons').split('\n')
+
+    subkeys = []
+    keys = []
+    for line in out:
+        if re.search('^ssb', line):
+            subkeys.append(line.split(':')[4])
+        elif re.search('^sec', line):
+            keys.append(line.split(':')[4])
+
+    return {'keys': keys, 'subkeys': subkeys }
+
+def get_public_keys():
+    """
+        Same as get_secret_keys, except for public keys.
+
+        return:
+            A dictionary containing subkey and key keys, with lists of each respective available key list.
+    """
+    out = get_output('gpg --list-keys --with-colons')
+
+    keys = []
+    subkeys = []
+
+    for line in out:
+        if re.search('^pub', line):
+            keys.append(line.split(':')[4])
+        elif re.search('^sub', line):
+            subkeys.append(line.split(':')[4])
+
+    return {'keys': keys, 'subkeys': subkeys }
+
+
+def run_proc(args, bufsize=0, executable=None, stdin=None, stdout=None, stderr=None, preexec_fn=None, close_fds=False, shell=False, cwd=None, env=None, universal_newlines=False, startupinfo=None, creationflags=0):
+
+    args = shlex.split(args)
+
+    return sp.Popen(args, bufsize, executable, stdin, stdout, stderr, preexec_fn, close_fds, shell, cwd, env, universal_newlines, startupinfo, creationflags)
+
+def get_output(args):
+    """
+        call subprocess.check_output with shlex'ed args.
+
+        params:
+            args: the command to execute
+
+        return:
+            the output of the command
+    """
+    args = shlex.split(args)
+
+    return sp.check_output(args)
