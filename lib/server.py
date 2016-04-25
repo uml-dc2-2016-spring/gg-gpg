2ac178f759bdd1eefb357157bc8e7b5fcb534b54
Matt Wolf <mattwollf@gmail.com>
2016-04-25 13:24:08 -0400

    server.py test code, deserializer args for handler

--- lib/server.py
+++ lib/server.py
@@ -1,36 +1,58 @@
 import SocketServer
 
 import util
+import multiprocessing as mp
 
-channels = []
+listening_channels = []
 
-class MyTCPHandler(SocketServer.BaseRequestHandler):
+class AppendingTCPHandler(SocketServer.BaseRequestHandler):
     """
     The RequestHandler class for our server.
 
     It is instantiated once per connection to the server, and must
     override the handle() method to implement communication to the
     client.
+
+    in handle, implement either append to text file or write new file.
     """
 
     def handle(self):
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(8193).strip()
-        out = util.run_piped_proc('gpg --quiet --decrypt', self.data)
-        with open("server.log", "a") as log:
-            log.write("{}: {}\n".format(self.client_address[0], out.decode()))
+        if self.server.deserialize:
+            out = self.server.deserialize(self.data)
+        out = self.data
+
+        with open(self.server.infile, 'a') as of:
+            of.write('%s: %s\n' % (self.client_address[0], out.decode()))
 
 
-def start_server(host, port, channel):
-    global channels
+def start_appender(host, port, channel, infile='in', deserializer=None):
+    import os
+    global listening_channels
 
+    if not os.path.isdir(channel):
+       util.create_channel(channel, os.getcwd(), infile)
     os.chdir(channel)
-    channels.append(channel)
 
-    server = SocketServer.TCPServer((host, port), MyTCPHander)
+    server = SocketServer.TCPServer((host, port), AppendingTCPHandler)
     server.allow_reuse_address=1
+    server.infile = infile
+    server.deserialize = deserializer
     server_proc = mp.Process(target=server.serve_forever)
     server_proc.daemon = True
     server_proc.start()
     return server_proc
+
+if __name__ == '__main__':
+    import time
+    host = 'localhost'
+    port = 9000
+    channel = 'testin'
+    ofile = 'out'
+
+    serv_proc = start_appender(host, port, channel, ofile)
+
+    time.sleep(60)
+
 
