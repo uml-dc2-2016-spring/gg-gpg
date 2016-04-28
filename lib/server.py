d9b85d30457f0009e667505fa25208bf6fec1b27
Matt Wolf <mattwollf@gmail.com>
2016-04-28 08:52:20 -0400

    tcp handlers implemented, init wrappers, multiprocessing wrappers

--- lib/server.py
+++ lib/server.py
@@ -1,11 +1,54 @@
 import SocketServer
+import os
+import multiprocessing
+import tempfile
+import datetime
+import socket
+import sys
 
 import util
-import multiprocessing
+import gpg
 
-listening_channels = []
+class AbstractTCPHandler(SocketServer.BaseRequestHandler):
+    """
+        class for shared methods between appending and separate file tcp handlers.
+    """
 
-class AppendingTCPHandler(SocketServer.BaseRequestHandler):
+    def recv_all_tmp_file(self):
+        """
+            receive all data and write it to a temporary file.
+
+            return: the temporary file containing the read socket.
+        """
+        tmp = tempfile.TemporaryFile()
+        print 'server created tmp file for receiving'
+        # while True:
+        #     data = self.request.recv(4096)
+        #     if not data: break
+        #     tmp.write(data)
+
+        data = self.request.recv(8192)
+        print 'server writing to temp file, finished reading from socket'
+        tmp.write(data)
+
+        print 'server got out of while loop in handler'
+        sys.stdout.flush()
+
+        tmp.seek(0)
+        print 'server %s' % tmp.read().decode()
+        sys.stdout.flush()
+        tmp.seek(0)
+
+        return tmp
+
+    def deserialize(self, data):
+        if self.server.deserializer:
+            return self.server.deserializer(data)
+        return data
+
+
+
+class AppendingTCPHandler(AbstractTCPHandler):
     """
     The RequestHandler class for our server.
 
@@ -18,43 +61,86 @@ class AppendingTCPHandler(SocketServer.BaseRequestHandler):
 
     def handle(self):
         # self.request is the TCP socket connected to the client
-        self.data = self.request.recv(8193).strip()
-        if self.server.deserialize:
-            out = self.server.deserialize(self.data)
-        else:
-            out = self.data
+        print 'server in appending handler'
+        with self.recv_all_tmp_file() as tmp:
 
-        with open(self.server.outfile, 'a') as of:
-            of.write('%s: %s\n' % (self.client_address[0], out.decode()))
+            sys.stdout.flush()
 
+            data = tmp.read()
 
-def start_appender(host, port, channel, outfile='out', deserializer=None):
-    import os
-    global listening_channels
+            data = self.deserialize(data)
 
-    if not os.path.isdir(channel):
-       util.create_channel(channel, os.getcwd(), None, outfile)
-    os.chdir(channel)
+            name = client_address[0] # set name to IP address first
 
-    server = SocketServer.TCPServer((host, port), AppendingTCPHandler)
-    server.allow_reuse_address=1
-    server.outfile = outfile
-    server.deserialize = deserializer
-    server_proc = multiprocessing.Process(target=server.serve_forever)
-    server_proc.daemon = True
-    server_proc.start()
-    print 'server started'
-    return server_proc
+            uid = gpg.get_packet_signer_id(data)
 
-if __name__ == '__main__':
-    import time
-    host = 'localhost'
-    port = 9000
-    channel = 'testin'
-    ofile = 'out'
+            time = datetime.datetime.now().strftime('%c')
 
-    serv_proc = start_appender(host, port, channel, ofile)
-
-    time.sleep(60)
+            with open(self.server.outfile, 'a') as of:
+                of.write('%s %s: %s\n' % (time, name, out.decode()))
 
 
+class SeparateFileTCPHandler(AbstractTCPHandler):
+    """
+        The connection handler for the server.
+
+        This one implements separate file saving per connection.
+    """
+    def handle(self):
+        print 'server in SeparateFileTCPHandler'
+        with self.recv_all_tmp_file() as tmp:
+            data = tmp.read()
+
+            print sys.stdout.flush()
+            data = self.deserialize(data)
+
+            filename = datetime.datetime.now().strftime('%Y%b%d_%H_%M_%S_%f')
+
+            #TODO test what happens if file already exists, add try/except block
+            with open(filename, 'wb') as f:
+                f.write(data)
+
+def init_server_from_config(name, config, outfile='out', deserializer=None):
+    """
+        initialize the server based on the config dictionary.
+
+        see init_server
+    """
+    if 'file_save' in config.keys():
+        return init_server(name, config, SeparateFileTCPHandler, outfile, deserializer)
+    else:
+        return init_server(name, config, AppendingTCPHandler, outfile, deserializer)
+
+def init_server(name,  config, TCPHandler, outfile='out', deserializer=None):
+    """
+        Initialize but do not start a log appending server.
+
+        params:
+            name: the channel name.
+            config: a dictionary generated from a config file.
+            TCPHandler: the tcp handler class the server calls when connected
+            outfile: the file to write received data to.
+            deserializer: the method to call to deserialize data.
+
+        return: A multiprocessing.Process, targetting the server.
+    """
+    if not 'incoming_port' in config.keys():
+        return None
+
+
+    # bind to all interfaces for listening.
+    host = '0.0.0.0'
+    port = int(config['incoming_port'])
+
+    listener = SocketServer.TCPServer((host, port), TCPHandler)
+    listener.allow_reuse_address=1 # in case server needs to be stopped and started.
+    listener.outfile = os.path.join(os.getcwd(), outfile) # for the handler to see.
+    listener.deserializer = deserializer
+    def start():
+        print 'starting server on port %s' % port
+        listener.serve_forever()
+
+    proc = multiprocessing.Process(target=start)
+    proc.daemon = True # this python process will try to clean up the server's process at exit.
+
+    return proc
